70d8fa5b53c0a7c889955929007e70041d1f825c
ckjolhede <ckjolhede@gmail.com>
2024-06-05 18:57:27 -0600

    complete'

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    newList = [num for num in num_list if (num % 2)== 0]
+    return newList
 
 def make_exclamation(sentence_list):
-    pass
+    sentences = [sentence + "!" for sentence in sentence_list]
+    return sentences
